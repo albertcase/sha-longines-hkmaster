f0d1b8707018173cf497b117f3e864b04b96dc50
Vincen Wen <vincen.wen@samesamechina.com>
2015-03-11 16:00:24 +0800

    no message

--- js/main.js
+++ js/main.js
@@ -115,16 +115,18 @@ function wechatShare(timestamp_val,signature_val){
 
 
 
+$(function(){
+    $(".clicklink").click(function(){
+         ga('send','event','auction', 'home' ,'点击进入');
+    })
 
 
-$(".clicklink").click(function(){
-     ga('send','event','auction', 'home' ,'点击进入');
+    $(".listinfo").click(function(){
+        ga('send','event','auction', 'newslist' ,$(this).index());
+    })
+ 
 })
 
-
-$(".listinfo").click(function(){
-    ga('send','event','auction', 'newslist' ,$(this).index());
-})
 
 
 
